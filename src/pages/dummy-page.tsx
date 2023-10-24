1c7a34eb7a72434ab9f6279026e0fa78d0aa920f
Pawel Morawski <me.pawel.morawski@gmail.com>
2023-10-24 14:00:26 +0100

    Add a dummy page

---
+++ src/pages/dummy-page.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+import styles from './index.module.css';
+
+export default function Dummy() {
+  return (
+    <>
+      <Head>
+        <title>dummy page NIHR Funding and Awards</title>
+        <meta name="description" content="" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <h1 className={styles.title}>
+            <span className={styles.pinkSpan}>NIHR</span> Funding and Awards
+          </h1>
+        </div>
+      </main>
+    </>
+  );
+}
